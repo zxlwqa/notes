77d5cf56c02a074865af61b75cea9bd035a4fb3e
科技-刘 <165335155+zxlwq@users.noreply.github.com>
2025-09-06 07:24:34 +0800

    Update NotesListPage.tsx

--- src/pages/NotesListPage.tsx
+++ src/pages/NotesListPage.tsx
@@ -464,7 +464,7 @@ const NotesListPage: React.FC = () => {
             </div>
 
             {/* 搜索框 */}
-            <div className="w-80 relative z-[500]">
+            <div className="w-80 relative z-[800]">
               <AdvancedSearch
                 notes={notes}
                 onSearch={handleSearch}
