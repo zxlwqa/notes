b90c6adb288ce57bc299a462d0a3d187f9cd5310
科技-刘 <165335155+zxlwq@users.noreply.github.com>
2025-09-09 06:49:21 +0800

    Update useLocalStorage.ts

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
-  // 获取初始值
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       const item = window.localStorage.getItem(key)
@@ -12,10 +11,8 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
     }
   })
 
-  // 设置值的函数
   const setValue = (value: T | ((val: T) => T)) => {
     try {
-      // 允许值是一个函数，这样我们就有了与useState相同的API
       const valueToStore = value instanceof Function ? value(storedValue) : value
       setStoredValue(valueToStore)
       window.localStorage.setItem(key, JSON.stringify(valueToStore))
@@ -26,3 +23,4 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
 
   return [storedValue, setValue] as const
 }
+
